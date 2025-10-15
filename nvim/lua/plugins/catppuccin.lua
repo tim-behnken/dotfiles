fd9490fead9a347117cf254e6812d2b37bc2ba59
Tim Behnken <tim.behnken@nbcuni.com>
2025-10-15 10:36:17 -0700

    change nvim separator color

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,12 +4,17 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "macchiato", -- Options: "latte", "frappe", "macchiato", "mocha"
+      flavour = "mocha", -- Options: "latte", "frappe", "macchiato", "mocha"
       background = {
         light = "latte",
         dark = "mocha",
       },
       transparent_background = false, -- Set to true for a transparent background
+      custom_highlights = function(colors)
+        return {
+          WinSeparator = { fg = colors.sapphire }, -- Change 'flamingo' to your desired Catppuccin color
+        }
+      end,
     })
     vim.cmd.colorscheme("catppuccin")
   end,
